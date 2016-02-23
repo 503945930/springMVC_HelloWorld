65c13abb69abe23120bab3e9e5938461df276ac5
little-wei <503945930@qq.com>
2016-02-23 11:18:32 +0800

    first commit

---
+++ src/main/java/cn/ucai/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package cn.ucai.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ * Created by Administrator on 2016/2/23.
+ */
+@Controller
+public class HelloWorld {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+   public String index(){
+        return  "index";
+    }
+}
